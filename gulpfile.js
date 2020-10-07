e34f4c67652df0496f85449f88a15fdfa1704654
alvinamri <alvin.amri@ogilvy.com.au>
2020-10-07 11:18:29 +0800

    Add inject meta tag with dynamic width & height document

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,9 @@ Auto-package banners.
       then compress each folder to zip.
 4. Done, file your package under 'dist' folder
 
+NB:
+Under head tag, put below comment code, it will auto-inject meta ad.size:
+<!--AD.SIZE-->
 */
 
 var gulp = require("gulp"),
@@ -14,20 +17,30 @@ var gulp = require("gulp"),
 const imagemin = require("gulp-imagemin");
 var zip = require("gulp-zip");
 var rename = require("gulp-rename");
+var replace = require('gulp-replace');
 var fs = require("fs");
 var path = require("path");
+var sizeOf = require("image-size");
+var clean = require('gulp-clean');
+const { dir } = require("console");
 
 var zipName = "JOBNUMBER_DCM";
 
 var scriptsPath = "./sources";
+var generatedPath = "./dist/generated";
 var backupImagePath = "./sources/backup";
 var webshotImagePath = "./dist/shot";
 var webshotQuality = 80;
 var minifiedImagesPath = "./dist/minified";
-var delayBeforeCapture = 3000;
+var delayBeforeCapture = 20000;
 var bannerSelector = "#adwrapper";
 var imageExt = "jpg";
 var distPath = "./dist";
+
+gulp.task('do:clean', function () {
+  return gulp.src([distPath+'',])
+    .pipe(clean({force: true}));
+});
 
 gulp.task("webshot", function () {
   return gulp
@@ -53,18 +66,42 @@ gulp.task("do:rename", function (done) {
   var folders = getFolders(scriptsPath);
   tasks = folders.map(function (folder) {
     gulp
-      .src([minifiedImagesPath + "/" + folder + "/*"], { base: minifiedImagesPath })
+      .src([minifiedImagesPath + "/" + folder + "/*"], {
+        base: minifiedImagesPath,
+      })
       .pipe(rename(folder + "." + imageExt))
       .pipe(gulp.dest(backupImagePath));
   });
   done();
 });
-
-gulp.task("do:zip", function (done) {
+gulp.task("to-generated", function (done) {
+  return gulp.src([scriptsPath+'/**'])
+        .pipe(gulp.dest(generatedPath));
+});
+gulp.task("inject-meta-ad-size", function (done) {
+  setTimeout(function () {
   var folders = getFolders(scriptsPath);
   tasks = folders.map(function (folder) {
+    dir_image = minifiedImagesPath + "/" + folder + "/index." + imageExt;
+    dir_ =  generatedPath + "/" + folder;
+    dir_html = dir_ + "/index.html";
+    if (fs.existsSync(dir_image)) {
+      var dimensions = sizeOf(dir_image);
+      var meta_tag = '<meta name="ad.size" content="width='+dimensions.width+',height='+dimensions.height+'" />';
+      gulp.src([dir_html])
+        .pipe(replace("<!--AD.SIZE-->", meta_tag))
+        .pipe(gulp.dest(generatedPath+ "/" + folder));
+    }
+  });
+  done();
+  }, 2000);
+});
+
+gulp.task("do:zip", function (done) {
+  var folders = getFolders(generatedPath);
+  tasks = folders.map(function (folder) {
     return gulp
-      .src([scriptsPath + "/" + folder + "/*"], { base: scriptsPath })
+      .src([generatedPath + "/" + folder + "/*"], { base: generatedPath })
       .pipe(zip(folder + ".zip"))
       .pipe(gulp.dest(distPath));
   });
@@ -72,13 +109,13 @@ gulp.task("do:zip", function (done) {
 });
 
 gulp.task("do:zip2", function (done) {
-  setTimeout(function() {
+  setTimeout(function () {
     return gulp
-    .src([distPath+"/*.zip"])
-    .pipe(zip(zipName + ".zip"))
-    .pipe(gulp.dest(distPath));
- }, 2000);
- done();
+      .src([distPath + "/*.zip"])
+      .pipe(zip(zipName + ".zip"))
+      .pipe(gulp.dest(distPath));
+  }, 2000);
+  done();
 });
 
 function getFolders(dir) {
@@ -94,4 +131,7 @@ function getFolders(dir) {
 4. Zip files
 */
 
-gulp.task("process", gulp.series("webshot","minify","do:rename", "do:zip", "do:zip2"));
+gulp.task(
+  "process",
+  gulp.series("do:clean", "webshot", "minify", "do:rename", "to-generated","inject-meta-ad-size", "do:zip", "do:zip2")
+);
